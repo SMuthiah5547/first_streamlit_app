b2385296cb2c34779f00700d34556dfc2e5e5a3b
SMuthiah5547 <72676014+SMuthiah5547@users.noreply.github.com>
2023-09-07 23:17:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,12 @@ import streamlit as st
 st.title ("My Parents New Healthy Diner")
 st.header("Breakfast Menu")
 st.markdown("Omega 3 & Blueberry Oatmeal")
+
+st.markdown("Kale, Spinach & Rocket Smoothie")
+
+st.markdown("Hard Boiled free-range Egg")
+
+
 st.subheader("this is the subheader")
 st.caption("this is the caption")
 st.code("x=2021")
