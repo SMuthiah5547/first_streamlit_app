54c7dc87bcc968648f8721680d5d8e139fa21807
SMuthiah5547 <72676014+SMuthiah5547@users.noreply.github.com>
2023-09-07 23:20:34 -0400

    Update streamlit_app.py
    Updated as text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,8 @@
 import streamlit as st
 
-
-
 st.title ("My Parents New Healthy Diner")
 st.header("Breakfast Menu")
 st.text("Omega 3 & Blueberry Oatmeal")
-
 st.text("Kale, Spinach & Rocket Smoothie")
-
 st.text("Hard Boiled free-range Egg")
 
-
-st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
